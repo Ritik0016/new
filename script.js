0539df48ca4a6e44174a19f47e4174e480b76adf
Ritik0016 <ritikmishra0016@gmail.com>
2024-06-03 14:52:11 +0530

    changes

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 var p = document.querySelector("#a")
+var p2 = document.querySelector("#c")
 var button = document.querySelector("#b")
 
 button.addEventListener("click", () => {
     if(navigator.geolocation){
         navigator.geolocation.getCurrentPosition((position) =>{
-            console.log(position);
+            p.textContent = position.coords.latitude,
+            p2.textContent = position.coords.longitude
+            console.log(position)
         },
         (error) => {
             console.log(error.message);
